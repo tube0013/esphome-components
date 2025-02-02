c7e4d91b56cfdd2df5ef1a7452f4d9966a0be9e3
tube0013 <wpoland@gmail.com>
2025-02-01 20:12:52 -0500

    Update cc2652_flasher_action.h

--- components/cc2652_flasher/cc2652_flasher_action.h
+++ components/cc2652_flasher/cc2652_flasher_action.h
@@ -9,18 +9,20 @@ namespace cc2652_flasher {
 /** 
  * CC2652FlasherAction
  *
- * This custom action calls the flash_firmware() method of your CC2652FlasherComponent.
+ * A custom action that calls the flash_firmware() method of a CC2652FlasherComponent.
  */
 class CC2652FlasherAction : public Action<> {
  public:
   explicit CC2652FlasherAction(CC2652FlasherComponent *component) { component_ = component; }
-  void play(ActionCall back) override {
+  
+  void play(ActionCall callback) override {
     if (component_ != nullptr) {
       component_->flash_firmware();
     }
     // Signal that the action is complete.
     callback();
   }
+  
  protected:
   CC2652FlasherComponent *component_;
 };
