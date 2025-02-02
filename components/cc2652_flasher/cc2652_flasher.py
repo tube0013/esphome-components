fdc4013a26bfdfc86df45de1f03e5035242e6de3
tube0013 <wpoland@gmail.com>
2025-02-01 19:25:23 -0500

    updates

--- components/cc2652_flasher/cc2652_flasher.py
+++ components/cc2652_flasher/cc2652_flasher.py
@@ -1,6 +1,6 @@
 import esphome.codegen as cg
 import esphome.config_validation as cv
-from esphome.components import uart, gpio
+from esphome.components import uart, output
 from esphome.const import CONF_ID, CONF_UART_ID
 
 # Define configuration keys.
@@ -9,8 +9,8 @@ CONF_BSL_OUTPUT = "bsl_output"
 CONF_RESET_OUTPUT = "reset_output"
 CONF_FLASHING_BAUD_RATE = "flashing_baud_rate"
 
-DEPENDENCIES = ["uart", "gpio"]
-AUTO_LOAD = ["uart", "gpio"]
+DEPENDENCIES = ["uart", "output"]
+AUTO_LOAD = ["uart", "output"]
 
 # Create a namespace for our component.
 cc2652_flasher_ns = cg.esphome_ns.namespace("cc2652_flasher")
@@ -19,34 +19,21 @@ CC2652FlasherComponent = cc2652_flasher_ns.class_(
 )
 
 # Define the configuration schema.
-CONFIG_SCHEMA = cv.Schema(
-    {
-        cv.GenerateID(): cv.declare_id(CC2652FlasherComponent),
-        cv.Required(CONF_MANIFEST_URL): cv.string,
-        cv.Required(CONF_UART_ID): cv.use_id(uart.UARTComponent),
-        cv.Required(CONF_BSL_OUTPUT): cv.use_id(gpio.GPIOBinaryOutput),
-        cv.Required(CONF_RESET_OUTPUT): cv.use_id(gpio.GPIOBinaryOutput),
-        cv.Optional(CONF_FLASHING_BAUD_RATE, default=500000): cv.positive_int,
-    }
-).extend(cv.COMPONENT_SCHEMA).extend(uart.UART_DEVICE_SCHEMA)
+CONFIG_SCHEMA = cv.Schema({
+    cv.GenerateID(): cv.declare_id(CC2652FlasherComponent),
+    cv.Required("manifest_url"): cv.string,
+    cv.Required(CONF_UART_ID): cv.use_id(uart.UARTComponent),
+    cv.Required("bsl_output"): cv.use_id(output.BinaryOutput),
+    cv.Required("reset_output"): cv.use_id(output.BinaryOutput),
+    cv.Optional("flashing_baud_rate", default=500000): cv.positive_int,
+}).extend(cv.COMPONENT_SCHEMA).extend(uart.UART_DEVICE_SCHEMA)
 
 def to_code(config):
-    # Create a new instance of the component.
     var = cg.new_Pvariable(config[CONF_ID])
-    
-    # Set the manifest URL.
-    cg.add(var.set_manifest_url(config[CONF_MANIFEST_URL]))
-    
-    # Set the flashing baud rate (e.g., 500000).
-    cg.add(var.set_flashing_baud_rate(config[CONF_FLASHING_BAUD_RATE]))
-    
-    # Resolve and assign the BSL GPIO output.
-    bsl = yield cg.get_variable(config[CONF_BSL_OUTPUT])
+    cg.add(var.set_manifest_url(config["manifest_url"]))
+    cg.add(var.set_flashing_baud_rate(config["flashing_baud_rate"]))
+    bsl = yield cg.get_variable(config["bsl_output"])
     cg.add(var.set_bsl_output(bsl))
-    
-    # Resolve and assign the reset GPIO output.
-    reset = yield cg.get_variable(config[CONF_RESET_OUTPUT])
+    reset = yield cg.get_variable(config["reset_output"])
     cg.add(var.set_reset_output(reset))
-    
-    # Register this component as a UART device, using the provided uart_id.
     yield uart.register_uart_device(var, config)
